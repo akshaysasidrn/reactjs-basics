f1f7baac6a4430b0047bdbfa864b86475cd7e0d1
Akshay <akshaysasidharan93@gmail.com>
2017-03-24 14:11:16 +0530

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World. </h1>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById("app"));
